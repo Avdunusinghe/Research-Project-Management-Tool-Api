6a3e9261231ed06cfc61da27ce94f3e09cd2d4d8
Avdunusinghe <avdunusinghe@gmail.com>
2022-04-01 19:20:44 +0530

    User Service Partilly Implemented

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,13 +3,15 @@ const bcrypt = require("bcrypt");
 
 const saveUser = async (request, response) => {
   try {
-    let { firstName, lastName, email, password } = request.body;
+    let { firstName, lastName, email, mobileNumber, password } = request.body;
 
     let user = new User({
       firstName,
       lastName,
       email,
+      mobileNumber,
       password,
+      mobileNumber,
     });
 
     const salt = await bcrypt.genSalt(10);
@@ -17,6 +19,13 @@ const saveUser = async (request, response) => {
     await user.save();
 
     response.status(200).send("User has been save Successfully");
+  } catch (error) {
+    response.status(400).json(error.message);
+  }
+};
+
+const getAllUsersDetails = (request, response) => {
+  try {
   } catch (error) {
     response.status(400).json(error.message);
   }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,14 +10,38 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  studentId: {
+    type: String,
+  },
   email: {
     type: String,
     required: true,
     unique: true,
   },
+  mobileNumber: {
+    type: String,
+    required: true,
+  },
   password: {
     type: String,
     required: true,
+  },
+  createOn: {
+    type: Date,
+    required: true,
+  },
+  updatedOn: {
+    type: Date,
+    required: true,
+  },
+  role: {
+    type: [
+      {
+        type: String,
+        enum: ["student", "admin", "lecurer"],
+        default: ["student"],
+      },
+    ],
   },
 });
 
