a939f799d0ef93a48e55a4fe80ca142c2828bb49
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2022-05-21 00:47:02 +0530

    conflict resolved

--- src/controllers/topic.controller.js
+++ src/controllers/topic.controller.js
@@ -1,30 +1,4 @@
 const Topic = require("../models/topic.model");
-<<<<<<< HEAD
-const bcrypt = require("bcrypt");
-
-const saveTopic = async (request, response) => {
-  try {
-    let {
-      id,
-      subjectId,
-      groupID,
-      groupName,
-      topicName,
-      studentId,
-      studentName,
-      studentEmail,
-    } = request.body;
-
-    if (id == null) {
-      let topic = new Topic({
-        subjectId,
-        groupID,
-        groupName,
-        topicName,
-        studentId,
-        studentName,
-        studentEmail,
-=======
 
 const registerTopic = async (request, response) => {
   try {
@@ -36,41 +10,10 @@ const registerTopic = async (request, response) => {
         subjectName,
         subjectId,
         groupId,
->>>>>>> 261d3a3a70d7a51d5d38aea107c0677bd613774a
         createOn: new Date().toUTCString(),
         updatedOn: new Date().toUTCString(),
       });
 
-<<<<<<< HEAD
-      const isSuccess = sendTopicRegisteredEmail(topicGroupDetails);
-
-      if (isSuccess) {
-        await topic.save();
-
-        response.status(200).send("Topic Group has been save Successfully");
-      } else {
-        response.status(400).json("Error,please contact Supervisor");
-      }
-    } else {
-      const isTopicAvailable = await Topic.findById(id);
-
-      if (!isTopicAvailable) {
-        return res.status(404).json("Cannot Find Student Group");
-      }
-
-      const topicObj = await Topic.findByIdAndUpdate(id, {
-        subjectId,
-        groupID,
-        groupName,
-        topicName,
-        studentId,
-        studentName,
-        studentEmail,
-        updatedOn: new Date().toUTCString(),
-      });
-
-      response.status(200).json("Topic Group has been  Update SuccessFully");
-=======
       if (isSuccess) {
         await topic.save();
 
@@ -94,109 +37,12 @@ const registerTopic = async (request, response) => {
       });
 
       response.status(200).json("Topic has been  Update SuccessFully");
->>>>>>> 261d3a3a70d7a51d5d38aea107c0677bd613774a
     }
   } catch (error) {
     response.status(400).json(error.message);
   }
 };
 
-<<<<<<< HEAD
-// const getAllTopicDetails = async (request, response) => {
-//   const limit = 0;
-//   const skip = 0;
-//   const totalRecordCount = 0;
-//   const totalPageCount = 0;
-//   const totalPages = 0;
-//   const page = 0;
-
-//   try {
-//     const { userRole, searchText } = request.query;
-
-//     let query = await Topic.find();
-
-//     if (userRole > 0) {
-//       query = await query.find(userRole);
-//     }
-//     if (searchText) {
-//       query = await query.find(searchText);
-//     }
-
-//     page = parseInt(request.query.page) || 1;
-//     limit = parseInt(request.query.limit) || 10;
-//     skip = (page - 1) * limit;
-
-//     query = query.skip(skip).limit(limit);
-
-//     const studentGroupDetails = await query;
-
-//     totalRecordCount = studentGroupDetails.length;
-//     totalPageCount = Math.ceil(totalRecordCount / limit);
-
-//     request.status(200).json({
-//       studentGroupDetails,
-//       totalRecordCount,
-//       totalPageCount,
-//     });
-//   } catch (error) {
-//     response.status(400).json(error.message);
-//   }
-// };
-
-const deleteTopic = async (request, response) => {
-  try {
-    const topicId = request.params.id;
-
-    let query = await Topic.findById(topicId);
-
-    if (!query) {
-      return response
-        .status(200)
-        .json("Cannot Find Topic,Please Try Again");
-    }
-
-    query = await Topic.findByIdAndDelete(topicId);
-
-    response.status(200).json("Topic has been delete successfully");
-  } catch (err) {
-    response.status(400).json(err.message);
-  }
-};
-
-const getTopicById = async (request, response) => {
-  try {
-    const topicId = request.params.id;
-    if (topicId != null) {
-      response.json(topic);
-    } else {
-      return response
-        .status(200)
-        .json("Cannot Find Topic,Please Try Again");
-    }
-  } catch (error) {
-    response.status(400).json(error.message);
-  }
-};
-
-const getAllTopics = async (request, response) => {
-  try {
-    const topicDetails = await Topic.find().select("-password");
-
-    response.json(topicDetails);
-  } catch (error) {
-    response.status(400).json(error.message);
-  }
-};
-
-module.exports = {
-  saveTopic,
-  //getAllTopicDetails,
-  deleteTopic,
-  getTopicById,
-  getAllTopics,
-};
-=======
 module.exports = {
   registerTopic,
 };
->>>>>>> 261d3a3a70d7a51d5d38aea107c0677bd613774a
--- src/routes/topic.route.js
+++ src/routes/topic.route.js
@@ -1,36 +1,5 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-const auth = require("../middleware/auth");
-const {
-    saveTopic,
-    //getAllTopicDetails,
-    deleteTopic,
-    getTopicById,
-    getAllTopics,
-} = require("../controllers/topic.controller");
-
-//@route POST api/topic/
-//@description Save topic
-router.post("/", saveTopic);
-
-//@route GET api/topic/getAllTopicDetails
-//@desc Get All Topics
-//router.get("/getAllTopicDetails", getAllTopicDetails);
-
-//@route GET api/topic/all
-//@desc Get All Topics
-router.get("/all", getAllTopics);
-
-//@route Delete api/topic/id
-//@desc Delete Topic
-router.delete("/:id", deleteTopic);
-
-//@route GET api/studentGroup/getTopicById/id
-//@desc GET TopicById
-router.get("/:id", getTopicById);
-module.exports = router;
-=======
 
 const { registerTopic } = require("../controllers/topic.controller");
 
@@ -39,4 +8,3 @@ const { registerTopic } = require("../controllers/topic.controller");
 router.post("/", registerTopic);
 
 module.exports = router;
->>>>>>> 261d3a3a70d7a51d5d38aea107c0677bd613774a
