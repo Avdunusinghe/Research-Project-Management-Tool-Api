4c3a96b081526476fe7d5b4dbda64498737323fe
Avdunusinghe <avdunusinghe@gmail.com>
2022-06-01 15:47:36 +0530

    Allocate Panel Member Implemented

--- src/utils/email.helper.js
+++ src/utils/email.helper.js
@@ -108,7 +108,61 @@ function sendStudentRegisteredEmail(studentDetails) {
 	return isSuccess;
 }
 
+//Send Panel Member Allocated Email
+function sendPanelMemberAllocateEmail(emailBodyDetails) {
+	let isSuccess = false;
+	try {
+		const emailSettings = getComapanyEmailSettings();
+
+		const SMTPTransporter = mailManager.createTransport({
+			host: emailSettings.SMTP_HOST,
+			port: emailSettings.SMTP_PORT,
+			service: emailSettings.SMTP_SERVICE,
+			auth: {
+				user: emailSettings.SMTP_USER,
+				pass: emailSettings.SMTP_PASSWORD,
+			},
+		});
+
+		SMTPTransporter.verify(function (error, success) {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log("Server is ready to take our messages");
+			}
+		});
+
+		const emailBody = {
+			from: emailSettings.SMTP_USER,
+			to: emailBodyDetails.email,
+			subject: "Panel Member Allocated",
+			text: "RPMT",
+			html: `<b>Hey Please Contact Your Group Panel Member</b>
+         <br> Panel Member Name ${emailBodyDetails.panelMemberName} and Email ${emailBodyDetails.panelMemberName} <br/>`,
+		};
+
+		SMTPTransporter.sendMail(
+			emailBody,
+			(error, info) => {
+				if (error) {
+					console.log(error);
+					isSuccess = false;
+				} else {
+					console.log("Email sent : " + info.response);
+					console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+					console.log("Message sent: %s", info.messageId);
+				}
+			},
+			(isSuccess = true)
+		);
+		return isSuccess;
+	} catch (error) {
+		console.log(error.message);
+	}
+	return isSuccess;
+}
 module.exports = {
 	sendUserRegisteredEmail,
 	sendStudentRegisteredEmail,
+	sendPanelMemberAllocateEmail,
 };
